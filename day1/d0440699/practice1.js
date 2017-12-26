a8af4c4c3cd23d619cd59c4cfedc12fab03f5e2b
d0440699 <zhiglasses@gmail.com>
2017-12-26 19:28:58 +0800

    up

---
+++ day1/d0440699/practice1.js
@@ -0,0 +1,23 @@
+{
+  "name": "d0440699",
+  "version": "1.0.0",
+  "description": "fcu",
+  "main": "index.js",
+  "scripts": {
+    "test": "echo \"Error: no test specified\" && exit 1",
+    "homework": "helloworld"
+  },
+  "author": "d0440699",
+  "license": "ISC",
+  "repository": {
+    "type": "git",
+    "url": "https://github.com/fcu-d0440699/"
+  },
+  "keywords": [],
+  "devDependencies": {
+    "mocha": "^4.0.1"
+  },
+  "dependencies": {
+    "axios": "^0.17.1"
+  }
+}
